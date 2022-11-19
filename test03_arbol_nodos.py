9bf5b6a1d9e203b33a568dec7fdf99bf849a5f7f
Sebastian Lopez Buritica <selobu@gmail.com>
2022-11-18 19:23:13 -0500

    add test 03 tree search

--- test03_arbol_nodos.py
+++ test03_arbol_nodos.py
@@ -3,51 +3,45 @@ class Node:
         self.value = value
         self._left = None
         self._right = None
-
+    @property
+    def hasLeft(self):
+        return self._left is not None
+    @property
+    def hasRigth(self):
+        return self._right is not None
+    @property
+    def left(self):
+        return self._left
+    @property
+    def right(self):
+        return self._right
     def set_left(self, left):
         self._left = left
         return self
-
     def set_right(self, right):
         self._right = right
         return self
 
-    @property
-    def left(self):
-        return self._left
-
-    @property
-    def rigth(self):
-        return self._right
-
-    @property
-    def hasleft(self):
-        if self._left is not None:
-            return True
-        return False
-
-    @property
-    def hasright(self):
-        if self._right is not None:
-            return True
-        return False
-
-    def __str__(self):
-        return f"{self.value}"
-
-
 def printTree(tree):
-    if tree.hasleft:
+    if tree.hasLeft:
         printTree(tree.left)
-    if tree.hasright:
-        printTree(tree.rigth)
+    if tree.hasRigth:
+        printTree(tree.right)
     print(tree.value)
 
-
-tree = (
-    Node(10)
-    .set_left(Node(5).set_left(Node(2)).set_right(Node(7).set_left(Node(5))))
-    .set_right(Node(23).set_left(Node(18)).set_right(Node(31)))
-)
+tree = Node(10)\
+    .set_left(
+        Node(5)\
+            .set_left(Node(2))\
+            .set_right(
+                Node(7)\
+                .set_left(Node(5))
+                )
+        )\
+    .set_right(
+        Node(23)\
+            .set_left(Node(18))\
+            .set_right(Node(31))
+        )
 
 printTree(tree)
